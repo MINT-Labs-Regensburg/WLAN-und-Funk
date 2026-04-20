db601cedccc18a18a3334a0d8f24e9f3105cc8b0
PeterRegensburg <peter.dirnhofer@t-online.de>
2026-04-20 18:11:07 +0200

    commented. send and receive

--- loesungen/01b_espnow_send_loesung.cpp
+++ loesungen/01b_espnow_send_loesung.cpp
@@ -2,10 +2,9 @@
 #include <WiFi.h>
 #include <esp_now.h>
 
-// Change this to receiver MAC address.
 
-
-// A4 F0 0F 77 7E C0 
+// Ersetze diese MAC-Adresse durch die tatsächliche MAC-Adresse deines Empfängers
+// Beispiel-MAC-Adresse des Empfängers: A4 F0 0F 77 7E C0 
 uint8_t receiverMac[] = {0xA4, 0xF0, 0x0F, 0x77, 0x7E, 0xC0};
 
 void setup() {
@@ -17,9 +16,14 @@ void setup() {
     while (true) delay(1000);
   }
 
+  // Erstelle eine Peer-Info-Struktur zur Registrierung des Empfängers
+  // peerInfo enthält Informationen über den Empfänger (wie die MAC-Adresse)
   esp_now_peer_info_t peerInfo = {};
+  // Kopiere die MAC-Adresse des Empfängers in die Peer-Info-Struktur
+  // Dies teilt ESP-NOW mit, wohin die Nachrichten gesendet werden sollen
   memcpy(peerInfo.peer_addr, receiverMac, 6);
 
+  // Registriere den Empfänger als Peer-Gerät in ESP-NOW
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Peer fail");
     while (true) delay(1000);
--- loesungen/01c_espnow_receive_loesung.cpp
+++ loesungen/01c_espnow_receive_loesung.cpp
@@ -2,38 +2,49 @@
 #include <WiFi.h>
 #include <esp_now.h>
 
-
-
+// Callback-Funktion, die automatisch aufgerufen wird, wenn eine ESP-NOW-Nachricht empfangen wird
+// Sie erhält Informationen über den Sender (info) und die empfangenen Daten (data)
 void onDataRecv(const esp_now_recv_info *info, const uint8_t *data, int len)
 {
+    // Gib "From: " aus, gefolgt von der MAC-Adresse des Senders
     Serial.print("From: ");
 
-    // sender MAC address
+    // MAC address des Senders ausgeben
     for (int i = 0; i < 6; i++)
     {
         Serial.printf("%02X", info->src_addr[i]);
-        if (i < 5) Serial.print(":");
+        if (i < 5)
+            Serial.print(":");
     }
 
+    // Daten vom Sender ausgeben
     Serial.print(" | Data: ");
-
+    // Schleife durch alle empfangenen Bytes vom Sender
     for (int i = 0; i < len; i++)
     {
-        if (data[i] == '\0') break;
+        // Breche ab, wenn ein Null-Terminator (Ende-Zeichen) gefunden wird
+        if (data[i] == '\0')
+            break;
+        // Gib das Byte als Zeichen aus
         Serial.print((char)data[i]);
     }
 
     Serial.println();
 }
 
+// Initialisierung des ESP32
 void setup()
 {
     Serial.begin(115200);
+    // Setze den WiFi-Modus auf Station (Empfänger-Modus)
     WiFi.mode(WIFI_STA);
+    // Starte den WiFi in Station-Modus
     WiFi.STA.begin();
+    // Zur Information: Die MAC-Adresse des ESP32 wird hier ausgegeben, damit du sie für den Sender verwenden kannst
     Serial.print("My MAC: ");
     Serial.println(WiFi.macAddress());
 
+    // Initialiere ESP-NOW
     if (esp_now_init() != ESP_OK)
     {
         Serial.println("ESP-NOW fail");
@@ -41,7 +52,11 @@ void setup()
             delay(1000);
     }
 
+    // Registriere die Callback-Funktion, die aufgerufen wird, wenn Daten empfangen werden
+    // onDataRecv wird automatisch aufgerufen, wenn eine ESP-NOW-Nachricht ankommt
     esp_now_register_recv_cb(onDataRecv);
+
+    // Bestätige, dass der Empfänger bereit ist
     Serial.println("Receive ready");
 }
 
